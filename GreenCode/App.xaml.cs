cd5c6c08c1869197c060ab9c89e63c958385c513
Yaka <Yaka@YAKAPAD>
2022-05-20 13:01:20 +0800

    Add project files.

---
+++ GreenCode/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GreenCode
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            if (Properties.ContainsKey("SName"))
+            {
+                ((MainPage)MainPage).resumeData();
+            }
+        }
+
+        protected override void OnSleep()
+        {
+            ((MainPage)MainPage).saveData();
+        }
+
+        protected override void OnResume()
+        {
+            ((MainPage)MainPage).resumeData();
+        }
+    }
+}
---
+++ GreenCode/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GreenCode
+{
+    public partial class MainPage : ContentPage
+    {
+        const string strName = "SName";
+        const string strPlace = "SPlace";
+        const string strFontSize = "SFontSize";
+        int[] intArray = {35,50,16,14,15};
+        public MainPage()
+        {
+            InitializeComponent();
+            lblDate.Text = DateTime.Now.ToString("M");
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                lblTime.Text = DateTime.Now.ToString("T");
+                return true;
+            });
+           
+        }  
+        public void saveData()
+        {
+            Application.Current.Properties[strName] = lblName.Text;
+            Application.Current.Properties[strPlace] = lblPlace.Text;
+            Application.Current.Properties[strFontSize] = sliderSize.Value;
+        }
+        public void resumeData()
+        {
+            lblDate.Text = DateTime.Now.ToString("M");
+            lblName.Text = (string)Application.Current.Properties[strName];
+            lblPlace.Text = (string)Application.Current.Properties[strPlace];
+            sliderSize.Value = (double)Application.Current.Properties[strFontSize];
+            if (lblPlace.Text.Length > 0)
+                txtPlace.Opacity = 0;
+            sliderSize.Opacity = 0;
+        }
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            txtPlace.Opacity = 0;
+            int intIndex = txtPlace.Text.LastIndexOf(' ');
+            if (intIndex == -1)
+                lblPlace.Text = txtPlace.Text;
+            else
+            {
+                lblPlace.Text = txtPlace.Text.Substring(0, intIndex);
+                lblName.Text = txtPlace.Text.Substring(intIndex);
+            }
+        }
+        void OnEntryFocused(object sender, EventArgs e)
+        {
+            txtPlace.Opacity = 1;
+            txtPlace.Text = "";
+        }
+        void OnSliderValueChanged(object sender, EventArgs e)
+        {
+            lblDate.FontSize = intArray[0] * sliderSize.Value;
+            lblTime.FontSize = intArray[1] * sliderSize.Value;
+            lblName.FontSize = intArray[2] * sliderSize.Value;  
+            lblShow.FontSize = intArray[3] * sliderSize.Value;
+            lblPlace.FontSize = intArray[4] * sliderSize.Value;
+        }
+        void OnSliderUnfocused(object sender, EventArgs e)
+        {
+            sliderSize.Opacity = 0;
+        }
+        void OnSliderFocused(object sender, EventArgs e)
+        {
+            sliderSize.Opacity = 1;
+        }
+    }
+}
